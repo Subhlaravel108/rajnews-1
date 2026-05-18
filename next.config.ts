8df3669017c93c1099ad9fadc767dcd6075d688d
shubhlaravel108 <laravel.developer.108@gmail.com>
2026-05-18 16:33:50 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [{ source: "/index", destination: "/", permanent: true }];
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
